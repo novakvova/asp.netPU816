cf04ed058056c2a354cf502d2ada1814e7a0697f
Volodymyr Novak <novakvova@gmail.com>
2020-07-14 09:15:33 +0300

    Working site

---
+++ Bicycle.Web/Controllers/ShopController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bicycle.Web.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Search()
+        {
+            return View();
+        }
+    }
+}
---
+++ Bicycle.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bicycle.Web.MvcApplication" Language="C#" %>
